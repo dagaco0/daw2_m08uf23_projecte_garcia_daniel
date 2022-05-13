eea4cfe579a58b95e9bcc5c22a1d26b17906a5fb
dagaco2122daw2 <dagaco2122daw2@protonmail.com>
2022-05-13 18:08:28 +0200

    Segon commit del projecte m08uf2uf3

--- index.php
+++ index.php
@@ -1,39 +1,25 @@
 <?php
-require 'vendor/autoload.php';
-use Laminas\Ldap\Ldap;
-ini_set('display_errors',0);
-if ($_GET['usr'] && $_GET['ou']){
-    $domini = 'dc=fjeclot,dc=net';
-    $opcions = [
-        'host' => 'zend-dagaco',
-        'username' => "cn=admin,$domini",
-        'password' => 'fjeclot',
-        'bindRequiresDn' => true,
-        'accountDomainName' => 'fjeclot.net',
-        'baseDn' => 'dc=fjeclot,dc=net',
-    ];
-    $ldap = new Ldap($opcions);
-    $ldap->bind();
-    $entrada='uid='.$_GET['usr'].',ou='.$_GET['ou'].',dc=fjeclot,dc=net';
-    $usuari=$ldap->getEntry($entrada);
-    echo "<b><u>".$usuari["dn"]."</b></u><br>";
-    foreach ($usuari as $atribut => $dada) {
-        if ($atribut != "dn") echo $atribut.": ".$dada[0].'<br>';
-    }
-}
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>
-MOSTRANT DADES D'USUARIS DE LA BASE DE DADES LDAP
-</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inici de sessió</title>
+    <style>
+        body{
+            font-family: Arial;
+        }
+    </style>
 </head>
 <body>
-<form action="http://zend-dagaco/zendldap" method="GET">
-Unitat organitzativa: <input type="text" name="ou"><br>
-Usuari: <input type="text" name="usr"><br>
-<input type="submit"/>
-<input type="reset"/>
-</form>
+    <form action="iniciarSessio.php" method="POST">
+        <label for="user">Usuari</label>
+        <input type="text" id="user" name="user" value="cn=admin,dc=fjeclot,dc=net" readonly><br>
+        <label for="pass">Contrasenya</label>
+        <input type="password" id="passowrd" name="password"><br>
+        <input type="submit" value="Iniciar sessió">
+    </form>
 </body>
 </html>
